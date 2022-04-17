0c24705c254d3aa8c5121b3de7eedf37cdc071a4
anwer25 <30316105+anwer25@users.noreply.github.com>
2022-04-17 21:45:10 +0100

    Update firebaseDB.ts

--- mobile/helthApp/database/firebaseDB.ts
+++ mobile/helthApp/database/firebaseDB.ts
@@ -1,16 +1,7 @@
 /*import firebase from '@react-native-firebase/app';
 import firestore from '@react-native-firebase/firestore';
 
-const firebaseConfig = {
-	apiKey: 'AIzaSyCh1bZAC95qYSW2u6H0ZjRV9qBltGs608M',
-	authDomain: 'helthapp-e8dcf.firebaseapp.com',
-	databaseURL: 'https://helthapp-e8dcf-default-rtdb.firebaseio.com',
-	projectId: 'helthapp-e8dcf',
-	storageBucket: 'helthapp-e8dcf.appspot.com',
-	messagingSenderId: '430124001949',
-	appId: '1:430124001949:web:8e17853e85d09f42180274',
-	measurementId: 'G-XQKS4ZDXJD',
-};
+const firebaseConfig = {};
 (async () => {
 	try {
 		if (!firebase.apps.length) {
